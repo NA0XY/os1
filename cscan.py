8ac90f59a78b5801e54f47577bc717b3df067d4f
Harsh Singhal <harshsinghal1972@gmail.com>
2025-05-06 15:14:42 +0530

    Update cscan.py

--- cscan.py
+++ cscan.py
@@ -1,35 +1,26 @@
-# cscan.py
-
 def run_cscan(requests, start, direction='right', max_cylinder=199):
     if not requests:
-        return [], 0, []
-
+        return [], 0
     requests = sorted(set(requests))
     left = [r for r in requests if r < start]
     right = [r for r in requests if r >= start]
     sequence = []
-    wrap_moves = []
-
     if direction == 'right':
         sequence.extend(right)
         if right and right[-1] != max_cylinder:
-            wrap_moves.append((right[-1], max_cylinder))
-        wrap_moves.append((max_cylinder, 0))
+            sequence.append(max_cylinder)
+        sequence.append(0)
         sequence.extend(left)
-    elif direction == 'left':
+    else:
         sequence.extend(reversed(left))
         if left and left[0] != 0:
-            wrap_moves.append((left[0], 0))
-        wrap_moves.append((0, max_cylinder))
+            sequence.append(0)
+        sequence.append(max_cylinder)
         sequence.extend(reversed(right))
-    else:
-        raise ValueError("Direction must be 'right' or 'left'")
-
-    full_sequence = [start] + sequence
+    sequence = [track for track in sequence if track not in [0, max_cylinder]]
     movement = 0
     pos = start
     for track in sequence:
         movement += abs(track - pos)
         pos = track
-
-    return sequence, movement, wrap_moves
+    return sequence, movement
