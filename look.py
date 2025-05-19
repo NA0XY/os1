590067cf83860dda6bf50e72e7800106e0479399
Harsh Singhal <harshsinghal1972@gmail.com>
2025-05-19 13:09:23 +0530

    Create look.py

---
+++ look.py
@@ -0,0 +1,30 @@
+def run_look(requests, start, direction):
+    requests = sorted(requests)
+    movement = 0
+    sequence = []
+    current = start
+
+    if direction == 'right':
+        right = [r for r in requests if r >= start]
+        left = [r for r in requests if r < start][::-1]
+        for r in right:
+            sequence.append(r)
+            movement += abs(current - r)
+            current = r
+        for r in left:
+            sequence.append(r)
+            movement += abs(current - r)
+            current = r
+    else:
+        left = [r for r in requests if r <= start][::-1]
+        right = [r for r in requests if r > start]
+        for r in left:
+            sequence.append(r)
+            movement += abs(current - r)
+            current = r
+        for r in right:
+            sequence.append(r)
+            movement += abs(current - r)
+            current = r
+
+    return sequence, movement
